9307c045f8cfc2c262e9cabbc9f501ab07ab18b6
Derpvader66 <111804053+Derpvader66@users.noreply.github.com>
2024-03-15 16:39:00 +0000

    Maybe

--- pages/0.1CLOSER.py
+++ pages/0.1CLOSER.py
@@ -2,15 +2,21 @@ import streamlit as st
 import pandas as pd
 import docx
 import fitz  # PyMuPDF
-from langchain.llms import OpenAI
-from langchain.chains import GenerationChain
+from langchain.llms import openai
+from langchain.chains import qa_generation  
+
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
 
 # Initialize Langchain with OpenAI
 with st.sidebar:
      openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
 "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     
-llm = OpenAI(api_key=openai_api_key)
+llm = openai(api_key=openai_api_key)
 generation_chain = GenerationChain(llm=llm)
 
 def read_pdf(file):
@@ -53,11 +59,24 @@ def upload_detailed_steps_documents():
     return detailed_docs
 
 def process_documents(business_process_doc, detailed_steps_docs):
-    combined_text = business_process_doc + "\n\n" + "\n\n".join(detailed_steps_docs.values())
-    prompt = f"Based on the following business process and detailed steps, generate comprehensive test cases:\n\n{combined_text}\n\nTest cases:"
-    response = generation_chain.run(prompt=prompt, max_tokens=1024)  # Adjust max_tokens as needed
-    test_cases = response.split('\n')  # Assuming each test case is separated by a newline
-    return test_cases
+    
+    prompt = ChatPromptTemplate.from_messages(
+    [("system", "Generate a test cases using the \n\n{context}")]
+)
+#    
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+    chain = create_stuff_documents_chain(llm, prompt)
+
+    docs = [Document(page_content= business_process_doc),
+            Document(page_content= detailed_steps_docs)]
+
+    chain.invoke({"context": docs})
+
+#    combined_text = business_process_doc + "\n\n" + "\n\n".join(detailed_steps_docs.values())
+#    prompt = f"Based on the following business process and detailed steps, generate comprehensive test cases:\n\n{combined_text}\n\nTest cases:"
+#    response = generation_chain.run(prompt=prompt, max_tokens=1024)  # Adjust max_tokens as needed
+#    test_cases = response.split('\n')  # Assuming each test case is separated by a newline
+#    return test_cases
 
 def display_test_cases(test_cases):
     st.subheader("Generated Test Cases")
@@ -79,7 +98,7 @@ def main():
 
     if st.button("Generate Test Cases") and business_process_doc and detailed_steps_docs:
         test_cases = process_documents(business_process_doc, detailed_steps_docs)
-        display_test_cases(test_cases)
+#        display_test_cases(test_cases)
 
 if __name__ == "__main__":
     main()
