4fef29d724e2c57df2df07363ce97e534ca17e31
Derpvader66 <111804053+Derpvader66@users.noreply.github.com>
2024-03-15 15:01:57 +0000

    DO MORE

--- pages/4_Langchain_PromptTemplate.py
+++ pages/4_Langchain_PromptTemplate.py
@@ -8,8 +8,8 @@ openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
 
 def blog_outline(topic):
-    # Instantiate LLM model
-    llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key="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")
+    # Instantiate LLM modelopenai_api_key 
+    llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key )
     # Prompt
     template = "As an experienced data scientist and technical writer, generate an outline for a blog about {topic}."
     prompt = PromptTemplate(input_variables=["topic"], template=template)
